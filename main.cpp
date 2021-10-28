32a9229d7166a420e7b09007231ab7bc677d637a
LeoDLC <leowowdlc@hotmail.com>
2021-10-27 19:10:45 -0500

    segundo commit - agrega la clase de powerdesigned

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+int main() {
+	cout<<"Hola mundo"<<endl;
 	return 0;
 }
