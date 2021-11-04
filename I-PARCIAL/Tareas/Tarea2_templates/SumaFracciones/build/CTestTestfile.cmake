ecf2212de4188dc4ac9ad9ef2bc0f35cb31fef32
Cristian Maranje Perez <cristianmaranje@gmail.com>
2021-11-03 22:03:27 -0500

    Tarea 2

---
+++ I-PARCIAL/Tareas/Tarea2_templates/SumaFracciones/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/u/progra/Estructura_de_Datos/2/Github grupo/Team-DataStrucure-7166/I-PARCIAL/Tareas/Tarea2_templates/SumaFracciones
+# Build directory: D:/u/progra/Estructura_de_Datos/2/Github grupo/Team-DataStrucure-7166/I-PARCIAL/Tareas/Tarea2_templates/SumaFracciones/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
