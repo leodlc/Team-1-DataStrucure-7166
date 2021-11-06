55f36becd5f423b5fec2d1cb558f39c0f0e6af8e
Johnny <jsloachamin1@espe.edu.ec>
2021-11-06 11:36:58 -0500

    Add Unit Test Proyect

---
+++ I-PARCIAL/Tareas/Tarea_3Complejo/ComplejoTest/ComplejoTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ComplejoTest
+{
+	TEST_CLASS(ComplejoTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
