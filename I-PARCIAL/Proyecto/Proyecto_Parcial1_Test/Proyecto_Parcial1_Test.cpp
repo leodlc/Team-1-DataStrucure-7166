3869c1ace9c6b93bae61b12949b375dc361469dc
Johnny <jsloachamin1@espe.edu.ec>
2021-11-23 22:07:44 -0500

    Unit Test Proyect

---
+++ I-PARCIAL/Proyecto/Proyecto_Parcial1_Test/Proyecto_Parcial1_Test.cpp
@@ -0,0 +1,80 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ProyectoParcial1Test
+{
+	TEST_CLASS(ProyectoParcial1Test1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+		TEST_METHOD(TestMethod2)
+		{
+
+		}
+		TEST_METHOD(TestMethod3)
+		{
+
+		}
+	};
+
+	TEST_CLASS(ProyectoParcial1Test2)
+	{
+	public:
+
+		TEST_METHOD(TestMethod2)
+		{
+
+		}
+		TEST_METHOD(TestMethod2)
+		{
+
+		}
+		TEST_METHOD(TestMethod3)
+		{
+
+		}
+	};
+
+	TEST_CLASS(ProyectoParcial1Test3)
+	{
+	public:
+
+		TEST_METHOD(TestMethod2)
+		{
+
+		}
+		TEST_METHOD(TestMethod2)
+		{
+
+		}
+		TEST_METHOD(TestMethod3)
+		{
+
+		}
+	};
+
+	TEST_CLASS(ProyectoParcial1Test4)
+	{
+	public:
+
+		TEST_METHOD(TestMethod2)
+		{
+
+		}
+		TEST_METHOD(TestMethod2)
+		{
+
+		}
+		TEST_METHOD(TestMethod3)
+		{
+
+		}
+	};
+
+}
