19eb4e483447edd81d61f29ffdd4944853b2846e
LeoDLC <leowowdlc@hotmail.com>
2021-12-20 04:29:05 -0500

    Taller actualizado

--- II-PARCIAL/Tareas/Expresiones lambda_ Deber2P2/Project1/Project1/lambda ejemplo.cpp
+++ II-PARCIAL/Tareas/Expresiones lambda_ Deber2P2/Project1/Project1/lambda ejemplo.cpp
@@ -18,7 +18,8 @@ int main()
 	vector<int> vec = {1,3,5,7,9,8,6,4,12};
 	int multi = 4;
 	sizeof tam;
-	tam = count_if(vec.begin(), vec.end(), [multi](int x) { return !(x % multi); });
+	tam = count_if(vec.begin(), vec.end(), [multi](int x) { 
+		return !(x % multi); });
 	cout << "enteros multiplos de " << multi << ": " << tam << endl;
 
 }
---
+++ II-PARCIAL/Tareas/Expresiones lambda_ Deber2P2/Project1/lambda ejemplo.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+class Multiplo {
+	int mul;
+
+public:
+	Multiplo(int multi):mul(multi){}
+	bool operator() (int x)
+	{
+		return !(x % mul);
+	}
+};
+int main()
+{
+	vector<int> vec = {1,3,5,7,9,8,6,4,12};
+	int multi = 4;
+	sizeof tam;
+	tam = count_if(vec.begin(), vec.end(), [multi](int x) { 
+		return !(x % multi); });
+	cout << "enteros multiplos de " << multi << ": " << tam << endl;
+
+}
